a531cda06c1878790f558b1e0ebb183e2b9febaf
하상혁 <72422020+gga01075@users.noreply.github.com>
2022-04-19 17:57:26 +0900

    Level1 이상한 문제 만들기 푸는중

--- Level1/이상한 문자 만들기.js
+++ Level1/이상한 문자 만들기.js
@@ -1,4 +1,25 @@
 function solution(s) {
     var answer = '';
+    let arr1 = s.split(" "); 
+    // console.log(arr1);
+    for(var i=0;i<arr1.length;i++){
+      let arr2 = arr1[i].split("");
+      console.log(`arr2 : ${arr2}`);
+      let arr3 = arr2.map((el, idx) => {
+          if(idx%2===0){
+            //   console.log(idx);
+              arr2[idx] = arr2[idx].toUpperCase();  
+          }else{
+            // console.log(idx);
+            arr2[idx] = arr2[idx].toLowerCase();
+          }
+          console.log(`arr2 : ${arr2}`);
+         return arr2;
+        });
+      arr3.join();
+      answer+= arr3+' '
+    }
     return answer;
 }
+
+console.log(solution("try hello world"));
