b925520715afc0f2b12139dbdea2542308548ed1
하상혁 <72422020+gga01075@users.noreply.github.com>
2022-04-26 17:19:30 +0900

    문자열 다루기 기본 푸는중...

--- Level1/문자열 다루기 기본.js
+++ Level1/문자열 다루기 기본.js
@@ -1,6 +1,8 @@
 /* 
 문제 설명
-문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+문자열 s의 길이가 4 혹은 6이고, 
+숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요.
+예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
 
 제한 사항
 s는 길이 1 이상, 길이 8 이하인 문자열입니다.
@@ -13,5 +15,14 @@ s	return
 
 function solution(s) {
   var answer = true;
-  return answer;
+  let reg = /[\D]/;
+  
+  return answer = !(reg.test(s));
 }
+
+/* 
+정규표현식으로 문자가 포함되어 있으면(test메서드)를 true를 반환하는데 !로 false를 answer에 대입해서 반환 
+
+*/
+// console.log(solution("a234"));
+console.log(solution("1234"));
