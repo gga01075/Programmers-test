1aa899e978f150ade5fae61f9d935f6482b1ef37
하상혁 <72422020+gga01075@users.noreply.github.com>
2022-04-27 16:50:15 +0900

    이상한 문자 만들기 푸는중...

--- Level1/이상한 문자 만들기.js
+++ Level1/이상한 문자 만들기.js
@@ -1,25 +1,29 @@
 function solution(s) {
-    var answer = '';
-    let arr1 = s.split(" "); 
-    // console.log(arr1);
-    for(var i=0;i<arr1.length;i++){
-      let arr2 = arr1[i].split("");
-    //   console.log(`arr2 : ${arr2}`);
-      let arr3 = arr2.map((el, idx) => {
-          if(idx%2===0){
-            //   console.log(idx);
-              arr2[idx] = arr2[idx].toUpperCase();  
+    
+  let arr = s.split(" ");
+// console.log(arr); //['try', 'hello', 'world']
+  let answer = arr.map((el) => {
+      //처음에 try가 들어감 (0,1,2)
+      //
+     for(let i=0;i<el.length;i++){
+       if(i%2===0){
+          //  console.log(el[i],'짝수');
+          el = el.replace(el[i], el[i].toUpperCase());
+          //  console.log(el, '짝수2');
           }else{
-            // console.log(idx);
-            arr2[idx] = arr2[idx].toLowerCase();
-          }
-          console.log(`arr2 : ${arr2}`);
-         return arr2;
-        });
-      arr3.join();
-      answer+= arr3+' '
-    }
-    return answer;
+            // console.log(el[i], '홀수');
+          el =  el.replace(el[i], el[i].toLowerCase());
+            // console.log(el, '홀수2');
+         }
+     }
+     
+     return el;
+    //  console.log(el);
+  });
+  // console.log(answer);
+  answer = answer.join(" ");
+  // console.log(answer);
+  return answer;
 }
 
-console.log(solution("try hello world"));
+solution(" try hello world");
